8e39acc9aea4b6597dfb940de499226fbb290eb8
ica29 <ica29@students.cs.ubc.ca>
2021-06-10 01:09:32 -0700

    added 8-14 stuff

---
+++ table_clericalstaff.sql
@@ -0,0 +1,6 @@
+CREATE TABLE ClericalStaff(
+	ID		    CHAR(5),
+	Password	VARCHAR(30) 	NOT NULL,
+	CName		VARCHAR(30)	    NOT NULL,
+	PRIMARY KEY (ID)
+);
---
+++ table_patient.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Patient(
+PersonalHealthNumber 	int,
+	PName			    VARCHAR(30)	    NOT NULL,
+	PSex				CHAR(1)		    NOT NULL,
+	StreetAddress		VARCHAR(30)	    NOT NULL,
+	PostalCode			CHAR(6)		    NOT NULL,
+	DateOfBirth			date			NOT NULL,
+    PRIMARY KEY (PersonalHealthNumber),
+	FOREIGN KEY (PostalCode) REFERENCES PatientAddress(PostalCode)
+};
