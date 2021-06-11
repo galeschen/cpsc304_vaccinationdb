5f7db1fb4823f1ad962fafb1f09e55d31c54e995
ica29 <ica29@students.cs.ubc.ca>
2021-06-10 22:13:03 -0700

    fixed almost all tables except for table_bookinginfo.sql

--- sql_tables/table_bookinginfo.sql
+++ sql_tables/table_bookinginfo.sql
@@ -3,7 +3,7 @@ CREATE TABLE BookingInfo(
     ClinicID		CHAR(5),
     BookerPHN		int		NOT NULL,
     PRIMARY KEY (AppointmentID, ClinicID),
-    FOREIGN KEY (AppointmentID) REFERENCES VaccinationAppointment,
+    FOREIGN KEY (AppointmentID) REFERENCES VaccinationAppointment(AppointmentID),
     FOREIGN KEY (ClinicID) REFERENCES Clinic(ClinicID),
     FOREIGN KEY (BookerPHN) REFERENCES Patient(personalHealthNumber)
 );
