51144ab821d957830aae388b2bfb4da2c0079055
Gale Chen <61856642+glchn@users.noreply.github.com>
2021-06-10 22:13:55 -0700

    Fixed inserts for 1-8

---
+++ sql_tables/insert_clinicaddress.sql
@@ -0,0 +1,14 @@
+INSERT INTO ClinicAddress
+VALUES ('V7M3E4', 'Richmond');
+
+INSERT INTO ClinicAddress
+VALUES ('V5X0A1', 'Vancouver');
+
+INSERT INTO ClinicAddress
+VALUES ('V5T4V5', 'Vancouver');
+
+INSERT INTO ClinicAddress
+VALUES ('V6G2L8', 'Burnaby');
+
+INSERT INTO ClinicAddress
+VALUES ('V5T4V5', 'Vancouver');
