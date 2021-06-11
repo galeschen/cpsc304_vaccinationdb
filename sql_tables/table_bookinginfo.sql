8add353ffe4c228d7cad5646b5df45fef2fb5298
ica29 <ica29@students.cs.ubc.ca>
2021-06-10 23:14:59 -0700

    cleaned up

--- sql_tables/table_bookinginfo.sql
+++ sql_tables/table_bookinginfo.sql
@@ -2,8 +2,8 @@ CREATE TABLE BookingInfo(
     AppointmentID	CHAR(5),
     ClinicID		CHAR(5),
     BookerPHN		int		NOT NULL,
-    PRIMARY KEY (AppointmentID, ClinicID),
-    FOREIGN KEY (AppointmentID) REFERENCES VaccinationAppointment(AppointmentID),
-    FOREIGN KEY (ClinicID) REFERENCES Clinic(ClinicID),
-    FOREIGN KEY (BookerPHN) REFERENCES Patient(personalHealthNumber)
+    PRIMARY KEY(AppointmentID, ClinicID),
+    FOREIGN KEY(AppointmentID) REFERENCES VaccinationAppointment(AppointmentID),
+    FOREIGN KEY(ClinicID) REFERENCES Clinic(ClinicID),
+    FOREIGN KEY(BookerPHN) REFERENCES Patient(personalHealthNumber)
 );
--- table_isallergicto.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IsAllergicTo(
-	IngredientName		    VARCHAR(30),
-	PersonalHealthNumber	int,
-	PRIMARY KEY (IngredientName, PersonalHealthNumber),
-	FOREIGN KEY (IngredientName) REFERENCES Ingredient(IName)
-		ON DELETE CASCADE,
-	FOREIGN KEY (PersonalHealthNumber) REFERENCES Patient(PersonalHealthNumber)
-        ON DELETE CASCADE,
-);
