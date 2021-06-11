3de968c20b3b5fb2757570e5138fe7702c0148d0
Gale Chen <61856642+glchn@users.noreply.github.com>
2021-06-10 22:05:17 -0700

    Create tables 1-8 working.

--- sql_tables/table_vaccine.sql
+++ sql_tables/table_vaccine.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Vaccine(
     ID              CHAR(5),
     VName           VARCHAR(30)     NOT NULL,
-    Cost            SMALLMONEY      NOT NULL,
+    Cost            DECIMAL      NOT NULL,
     VAvailability   INT             NOT NULL,   
     UNIQUE (VName),
     PRIMARY KEY (ID)
