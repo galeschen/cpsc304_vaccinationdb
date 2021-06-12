6b8e5b78e3f7d33ef79dfc457a1ddc25cc118fb7
UBC Student <wangkj20@students.cs.ubc.ca>
2021-06-11 19:27:32 -0700

    change table_clinic

--- sql_tables/table_clinic.sql
+++ sql_tables/table_clinic.sql
@@ -3,18 +3,11 @@ CREATE TABLE Clinic(
     ManagerID       CHAR(5),
     StreetAddress   VARCHAR(30)     NOT NULL,
     PostalCode      CHAR(6),
-<<<<<<< Updated upstream
     City            VARCHAR(30),
     ClinicName      VARCHAR(30)     NOT NULL,
     UNIQUE      (StreetAddress, ClinicName),
     PRIMARY KEY (ClinicID),
     FOREIGN KEY (PostalCode,City) REFERENCES ClinicAddress,
-=======
-    ClinicName      VARCHAR(30)     NOT NULL,
-    UNIQUE      (StreetAddress, ClinicName),
-    PRIMARY KEY (ClinicID),
-    FOREIGN KEY (PostalCode) REFERENCES ClinicAddress,
->>>>>>> Stashed changes
     FOREIGN KEY (ManagerID) REFERENCES Manager(ID)
         ON DELETE CASCADE
 );
--- sql_tables/table_clinicaddress.sql
+++ sql_tables/table_clinicaddress.sql
@@ -1,9 +1,5 @@
 CREATE TABLE ClinicAddress(
     PostalCode	CHAR(6),
 	City		VARCHAR(30),
-<<<<<<< Updated upstream
 	PRIMARY KEY (PostalCode, City)
-=======
-	PRIMARY KEY (PostalCode)
->>>>>>> Stashed changes
 );
