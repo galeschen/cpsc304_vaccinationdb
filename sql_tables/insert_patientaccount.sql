325f8b946048042d516af3cf12489c27ff594480
UBC Student <wangkj20@students.cs.ubc.ca>
2021-06-11 19:39:09 -0700

    Merge branch 'ianbranch'

---
+++ sql_tables/insert_patientaccount.sql
@@ -0,0 +1,29 @@
+INSERT INTO PatientAccount
+VALUES (‘AdultUsername42’, ‘passwordpepe’,3234842);
+
+INSERT INTO PatientAccount
+VALUES (‘AdultUsername52’, ‘onetwothree’,3425352);
+
+INSERT INTO PatientAccount
+VALUES (‘AdultUsername84’, ‘123pass123word’,3927384);
+
+INSERT INTO PatientAccount
+VALUES (‘AdultUsername23’, ‘genericpassword’,3304923);
+
+INSERT INTO PatientAccount
+VALUES (‘AdultUsername71’, ‘plsimgonandie’,2309571);
+
+INSERT INTO PatientAccount
+VALUES (‘MinorUsername34’, ‘password32155’,0592834);
+
+INSERT INTO PatientAccount
+VALUES (‘MinorUsername32’, ‘eggboy!394’,0984632);
+
+INSERT INTO PatientAccount
+VALUES (‘MinorUsername34’, ‘password099’,0572712);
+
+INSERT INTO PatientAccount
+VALUES (‘MinorUsername83’, ‘hellothere123’,0820183);
+
+INSERT INTO PatientAccount
+VALUES (‘MinorUsername73’, ‘goodbye5314’,0829573);
