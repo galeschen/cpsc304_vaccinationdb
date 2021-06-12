c52feee41b85813e185637ee4d23e359f6eaa26f
UBC Student <wangkj20@students.cs.ubc.ca>
2021-06-11 19:26:35 -0700

    change table_clinic

--- sql_tables/table_clinicaddress.sql
+++ sql_tables/table_clinicaddress.sql
@@ -1,5 +1,9 @@
 CREATE TABLE ClinicAddress(
     PostalCode	CHAR(6),
 	City		VARCHAR(30),
+<<<<<<< Updated upstream
 	PRIMARY KEY (PostalCode, City)
+=======
+	PRIMARY KEY (PostalCode)
+>>>>>>> Stashed changes
 );
