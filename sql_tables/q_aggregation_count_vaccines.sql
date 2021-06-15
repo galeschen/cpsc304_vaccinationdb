420795b9a69ac62bd868ccb4d39598397f9cf47e
UBC Student <wangkj20@students.cs.ubc.ca>
2021-06-14 23:26:45 -0700

    add css files

--- sql_tables/q_aggregation_count_vaccines.sql
+++
@@ -1,8 +0,0 @@
-/* CRITERIA:
-Aggregation Query
-Pick one query that requires the use of a distinct aggregation operator (min, max, average, or count are all fine).
-*/
-
-/* Returns the number of vaccines in the Vaccines table */
-SELECT COUNT(*)
-FROM Vaccine;
