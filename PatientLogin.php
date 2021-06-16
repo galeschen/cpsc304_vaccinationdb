df820c7c5a91cf41aab44b206f990ab39f9fa867
UBC Student <wangkj20@students.cs.ubc.ca>
2021-06-16 15:03:43 -0700

    Nurse UI and bug fixed

--- PatientLogin.php
+++ PatientLogin.php
@@ -55,6 +55,7 @@
                 echo "Patient username cannot be found";
             // if the inserted password matches the password associated w/ the given username...
             } else if ($password == $correctpassword[0]) {
+                disconnectFromDB();
                 header("Location: PatientHome.php?pusername=".$username);
                 exit();
             } else {
